0c3c65668b142926b92cc86cd75c21530d4d3e36
Ane Marinheiro Weber <anemariweber@gmail.com>
2025-05-06 22:18:46 +0200

    UseCase - Delete a Question Comment

--- test/repository/in-memory-question-comments-repository.ts
+++ test/repository/in-memory-question-comments-repository.ts
@@ -2,11 +2,29 @@ import { QuestionCommentsRepository } from "@/domain/forum/application/repositor
 import { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment";
 
 export class InMemoryQuestionCommentsRepository
-   implements QuestionCommentsRepository
- {
-   public items: QuestionComment[] = []
- 
-   async create(questionComment: QuestionComment) {
-     this.items.push(questionComment)
-   }
- }
+  implements QuestionCommentsRepository
+{
+  public items: QuestionComment[] = []
+
+  async findById(id: string) {
+  const questionComment = this.items.find((item) => item.id.toString() === id)
+
+  if (!questionComment) {
+    return null
+  }
+
+  return questionComment
+}
+
+  async create(questionComment: QuestionComment) {
+    this.items.push(questionComment)
+  }
+
+  async delete(questionComment: QuestionComment) {
+  const itemIndex = this.items.findIndex(
+    (item) => item.id === questionComment.id,
+  )
+
+  this.items.splice(itemIndex, 1)
+}
+}
